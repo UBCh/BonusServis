4e304392476f358ba0b2fea2557de0cb453df311
UBCh <u2b127@gmail.ru>
2021-10-11 18:53:56 +0300

    fixed #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-    service.calculate(amount, registered);
+    long expected = 30;
+    long actual = service.calculate(amount, registered);
+    boolean passed = expected == actual;
+    System.out.println(passed);
+
 
   }
 }
